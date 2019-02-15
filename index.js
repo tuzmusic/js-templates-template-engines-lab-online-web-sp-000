aad4cd30c6b55006dd65748250c85c42c7d30d6a
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-15 14:26:36 -0500

    prevent adding extra headers, but allow extra posts

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+function addPage() {
+  let pageTemplate = $('#page-template')[0].innerHTML
+  let pageContainer = $('#page-container')[0]
+  let pageTemplateFn = _.template(pageTemplate)
+  let templateHTML = pageTemplateFn()
+  pageContainer.innerHTML += templateHTML
+}
+
+
 function createPost() {
+  let pageContainer = $('#page-container')[0]
+  let len = pageContainer.innerHTML.trim().length
+  console.log(len);
+  if (len === 0) {
+    addPage()
+    // return
+  }  
   let title = $('#post-title')[0].value
   let body = $('#post-body')[0].value
   let author = $('#post-author')[0].value
 
-  let pageTemplate = $('#page-template')[0].innerHTML
   let postTemplate = $('#post-template')[0].innerHTML
   let postTemplateFn = _.template(postTemplate)
   let templateHTML = postTemplateFn({title:title, author:author, body:body})
 
   let postContainer = $('#post-container')[0]
   postContainer.innerHTML += templateHTML
+}
+
+function postComment() {
+  debugger
 }
